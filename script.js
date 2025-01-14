2f67242d76cdecae52d2a4426291aeb68c3f56a9
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-14 10:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-// const userResponse= confirm("Do you want to proceed?");
+const userResponse= confirm("Do you want to proceed?");
 
 if(userResponse){
 	alert("You clicked OK. Proceeding...");
